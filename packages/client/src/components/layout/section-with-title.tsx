e0c4d176eb6c61eb1b89739bc746304167d0b5dd
marcelchukwuma <marcel@khmtechnology.com>
2026-06-17 22:26:00 +0100

    fix

--- packages/client/src/components/layout/section-with-title.tsx
+++ packages/client/src/components/layout/section-with-title.tsx
@@ -1,19 +1,21 @@
 import React, { type PropsWithChildren } from 'react';
+import { cn } from '../../lib/utils';
 
 interface Props extends PropsWithChildren {
+  className?: string;
+  description?: string;
   legend: string;
   title: string;
-  description: string;
 }
 
-const SectionWithTitle: React.FC<Props> = ({ children, legend, title, description }) => {
+const SectionWithTitle: React.FC<Props> = ({ children, className, legend, title, description }) => {
   return (
     <React.Fragment>
-      <section className="bg-background py-28 px-20">
+      <section className={cn('bg-background py-28 px-20', className)}>
         <div className="flex flex-col items-center justify-center">
           <div className="text-[10px] text-secondary uppercase font-semibold">{legend}</div>
           <h2 className="max-w-213 text-accent font-bold capitalize text-5xl my-4">{title}</h2>
-          <p className="text-muted-foreground text-base max-w-145">{description}</p>
+          {description && <p className="text-muted-foreground text-base max-w-145">{description}</p>}
         </div>
 
         <div className="w-full">{children}</div>
