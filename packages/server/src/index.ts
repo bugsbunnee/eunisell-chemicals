a745c383f56f3f5e3ea0c885db6435a83144091c
marcelchukwuma <marcel@khmtechnology.com>
2026-07-30 10:21:10 +0100

    removing-clutter

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,16 +1,14 @@
 import 'dotenv/config';
 
+import express from 'express';
 import logger from './startup/logger.ts';
 import registerApp from './startup/index.ts';
 
 const app = registerApp();
+const port = Number(process.env.PORT) || 8080;
 
-if (!process.env.VERCEL) {
-  const port = Number(process.env.PORT) || 8080;
-
-  app.listen(port, () => {
-    logger.info(`Server running on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`);
+});
 
 export default app;
